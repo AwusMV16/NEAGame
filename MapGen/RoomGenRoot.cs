8cf58983afb02206273818cb441e83e65aaf6dd5
AwusMV16 <4036awab19@mv16.org.uk>
2025-11-26 10:43:05 +0000

    small changes

--- MapGen/RoomGenRoot.cs
+++ MapGen/RoomGenRoot.cs
@@ -6,17 +6,23 @@ public class RoomGenRoot : MonoBehaviour
     [SerializeField] private GameObject roomGeneratorPrefab;
     [SerializeField] private GameObject nextArea;
     [SerializeField] private int TunnelLength = 5;
+
     void Start()
     {
         RoomGen roomGenScript = GetComponentInChildren<RoomGen>(); 
         roomGenScript.remainingChildRooms = TunnelLength;
         roomGenScript.IsStartingRoom = true;
     }
+    
     void Update()
     {
         if (transform.childCount == 0)
         {
-            GameObject roomGenerator = Instantiate(roomGeneratorPrefab, transform.position, quaternion.identity, transform);
+            GameObject roomGenerator = Instantiate( 
+                roomGeneratorPrefab, 
+                transform.position, 
+                quaternion.identity, 
+                transform);
             RoomGen roomGenScript = roomGenerator.GetComponent<RoomGen>();
             roomGenScript.remainingChildRooms = TunnelLength;
             roomGenScript.IsStartingRoom = true;
